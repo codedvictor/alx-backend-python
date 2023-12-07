eba2fb27f0aa6a4dda1ebb59233bbf8066659533
codedvictor <victorkoladeige@gmail.com>
2023-12-07 13:52:42 +0100

    float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function to return string
+"""
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    function to convert float to the string
+    """
+    return str(n)
