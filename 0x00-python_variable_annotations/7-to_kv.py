4faebefd784dc77b609b7e31adc7d0804f71922c
codedvictor <victorkoladeige@gmail.com>
2023-12-07 16:24:35 +0100

    import math power

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,10 +3,11 @@
 a type-annotated function to_kv
 """
 from typing import Union, Tuple
+from math import pow
 
 
 def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
     """
     convert to str & int|float to tuple
     """
-    return (k, v*v)
+    return (k, pow(v, 2))
