410dbf9077e5cafe8af6ab5721cac0b25d7fec35
codedvictor <victorkoladeige@gmail.com>
2023-12-07 15:52:09 +0100

    fix bug

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from typing import List
 """
 function sumlist
 """
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     sum list of float
     """
