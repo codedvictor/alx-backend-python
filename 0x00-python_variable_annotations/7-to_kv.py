bf2ee5066503beffb749c775c8694046c475d10e
codedvictor <victorkoladeige@gmail.com>
2023-12-07 16:55:50 +0100

    fix bug

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,11 +3,10 @@
 a type-annotated function to_kv
 """
 from typing import Union, Tuple
-from math import pow
 
 
-def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     convert to str & int|float to tuple
     """
-    return (k, pow(v, 2))
+    return (k, v*v)
