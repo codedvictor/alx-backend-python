a32a4ee9b6ce6e5291c1839f382eac6d4093af1a
codedvictor <victorkoladeige@gmail.com>
2023-12-11 21:29:29 +0100

    fix pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import asyncio
 import random
+"""
+basic sync
+"""
+
 
 async def wait_random(max_delay: int = 10) -> float:
+    """wait random"""
     pend = random.uniform(0, max_delay)
     await asyncio.sleep(pend)
     return pend
