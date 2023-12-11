c932fd5945016c71a75d52719964985bbbaac959
codedvictor <victorkoladeige@gmail.com>
2023-12-11 23:25:59 +0100

    last bug fixed

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,9 +4,7 @@ Tasks
 """
 
 import asyncio
-from typing import List
-from random import uniform
-
+import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -14,6 +12,3 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     """Create an asyncio.Task for wait_random function."""
     task = asyncio.create_task(wait_random(max_delay))
     return task
-
-if __name__ == "__main__":
-    asyncio.run(test(5))
