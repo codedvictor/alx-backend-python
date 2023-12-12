d640e04e1d665112726209e3d6569658d3db18cb
codedvictor <victorkoladeige@gmail.com>
2023-12-12 11:25:32 +0100

    async generator using random module

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Async generator
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """random number generator"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
