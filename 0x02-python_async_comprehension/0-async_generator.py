97e2052a92b167d1329df66bb04f0bb23432ae90
codedvictor <victorkoladeige@gmail.com>
2023-12-12 12:36:21 +0100

    introduce random.random

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,13 @@ Async generator
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """random number generator"""
-    for i in range(10):
+
+    for _ in range(10):
+        """range"""
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
